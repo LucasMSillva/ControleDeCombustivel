a3de0c702d5b04713917e888d5a7bc6d2255a9b2
Lucas Morais Silva <71843447+LucasMSillva@users.noreply.github.com>
2021-06-11 15:48:58 -0300

    criando menu
    criando primeira parte do menu

--- controle/src/app/app.module.ts
+++ controle/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './feature/historicoFeature/cadastro/cadastro.component';
-import { ComponentsComponent } from './core/components/components.component';
 import { MenuComponent } from './core/component/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroComponent,
-    ComponentsComponent,
     MenuComponent
   ],
   imports: [
